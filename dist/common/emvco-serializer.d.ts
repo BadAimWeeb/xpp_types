851627d584e234bc19fc6afdd14cc95400626a95
BAW_CI_AUTOMATION <noreply+bot@gamestorevn.com>
2024-02-12 09:06:04 +0000

    Publish automated build result from 1.0.0+a718d02 main

--- dist/common/emvco-serializer.d.ts
+++ dist/common/emvco-serializer.d.ts
@@ -1,3 +1,4 @@
 type NestedRecord = [string, string | number | NestedRecord][];
 export declare function serialize(obj: NestedRecord): string;
+export declare function deserialize(data: string): NestedRecord;
 export {};
