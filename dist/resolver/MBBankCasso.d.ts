7c7a0d567a3697d97b4a953021cca97623792f1e
BAW_CI_AUTOMATION <noreply+bot@gamestorevn.com>
2024-02-14 15:21:08 +0000

    Publish automated build result from 1.0.0+5cdd04a main

---
+++ dist/resolver/MBBankCasso.d.ts
@@ -0,0 +1,35 @@
+import Base from "./Base.js";
+export default class VietinbankCasso extends Base<{
+    account: string;
+    timeout: number;
+    secretKey: string;
+    listenPath?: string;
+}, {}, {}> {
+    type: string;
+    type1: string;
+    type2: string;
+    name: string;
+    codename: string;
+    state: {
+        account: string;
+    };
+    createPaymentInstruction(appShort: string, appPaymentID: string, localID: string, amount: number): {
+        account: string;
+        amount: number;
+        message: string;
+        timeout: number;
+        qr: string;
+        bank: string;
+        vqrDeeplink: string;
+        localID: string;
+    };
+    knownTransactions: Set<string>;
+    readonly defaultDBValue: {
+        account: string;
+        secretKey: string;
+        listenPath: string;
+        timeout: number;
+    };
+    init(): Promise<void>;
+    close(): void;
+}
