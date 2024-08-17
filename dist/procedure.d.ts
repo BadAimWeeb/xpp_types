ca5ab75d0201bbfed3c146dfe7e1f42943f9fa49
BAW_CI_AUTOMATION <noreply+bot@gamestorevn.com>
2024-08-17 02:02:16 +0000

    Publish automated build result from 1.0.0+3d94125 main

--- dist/procedure.d.ts
+++ dist/procedure.d.ts
@@ -9,22 +9,28 @@ export declare const p: {
     input: <TIn>(parser: {
         parse: (input: unknown) => TIn;
     }) => {
-        resolve: <TOut>(oCallback: (gState: import("@badaimweeb/js-dtsocket").GetTypeContext<ServerContext<GlobalState, LocalState, EventTable, Session<WebSocket & {
+        resolve: <TOut>(oCallback: (gState: GlobalState, lState: Partial<LocalState>, input: TIn, socket: DTSocketServer_CSocket<ServerContext<GlobalState, LocalState, EventTable, Session<WebSocket & {
             req: IncomingMessage;
-        }>>, import("@badaimweeb/js-dtsocket").SymbolGlobalStateType>, lState: Partial<import("@badaimweeb/js-dtsocket").GetTypeContext<ServerContext<GlobalState, LocalState, EventTable, Session<WebSocket & {
-            req: IncomingMessage;
-        }>>, import("@badaimweeb/js-dtsocket").SymbolLocalStateType>>, input: TIn, socket: DTSocketServer_CSocket<ServerContext<GlobalState, LocalState, EventTable, Session<WebSocket & {
-            req: IncomingMessage;
-        }>>>) => TOut | PromiseLike<TOut>) => import("@badaimweeb/js-dtsocket").Procedure<TIn, TOut, ServerContext<GlobalState, LocalState, EventTable, Session<WebSocket & {
+        }>>, {
+            cs: {} & {
+                "internal:drop": [];
+            };
+            sc: {
+                payment: [appID: string, xppID: string, status: "success" | "failed", output: any, reason?: string | undefined];
+            };
+        }>) => TOut | PromiseLike<TOut>) => import("@badaimweeb/js-dtsocket").Procedure<TIn, TOut, ServerContext<GlobalState, LocalState, EventTable, Session<WebSocket & {
             req: IncomingMessage;
         }>>>;
-        streamResolve: <TOut>(oCallback: (gState: import("@badaimweeb/js-dtsocket").GetTypeContext<ServerContext<GlobalState, LocalState, EventTable, Session<WebSocket & {
+        streamResolve: <TOut>(oCallback: (gState: GlobalState, lState: Partial<LocalState>, input: TIn, socket: DTSocketServer_CSocket<ServerContext<GlobalState, LocalState, EventTable, Session<WebSocket & {
             req: IncomingMessage;
-        }>>, import("@badaimweeb/js-dtsocket").SymbolGlobalStateType>, lState: Partial<import("@badaimweeb/js-dtsocket").GetTypeContext<ServerContext<GlobalState, LocalState, EventTable, Session<WebSocket & {
-            req: IncomingMessage;
-        }>>, import("@badaimweeb/js-dtsocket").SymbolLocalStateType>>, input: TIn, socket: DTSocketServer_CSocket<ServerContext<GlobalState, LocalState, EventTable, Session<WebSocket & {
-            req: IncomingMessage;
-        }>>>) => AsyncIterable<TOut>, burst?: boolean) => import("@badaimweeb/js-dtsocket/dist/procedures.js").StreamingProcedure<TIn, TOut, ServerContext<GlobalState, LocalState, EventTable, Session<WebSocket & {
+        }>>, {
+            cs: {} & {
+                "internal:drop": [];
+            };
+            sc: {
+                payment: [appID: string, xppID: string, status: "success" | "failed", output: any, reason?: string | undefined];
+            };
+        }>) => AsyncIterable<TOut>, burst?: boolean) => import("@badaimweeb/js-dtsocket/dist/procedures.js").StreamingProcedure<TIn, TOut, ServerContext<GlobalState, LocalState, EventTable, Session<WebSocket & {
             req: IncomingMessage;
         }>>>;
     };
